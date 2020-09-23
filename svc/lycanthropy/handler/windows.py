0eeca77120c2856178af0937eabc7e12df5cfb2d
kryptops <kryptops@protonmail.com>
2020-09-23 00:13:50 -0400

    bug fixes for dist, api task deletion, windows task mapping

--- svc/lycanthropy/handler/windows.py
+++ svc/lycanthropy/handler/windows.py
@@ -7,61 +7,61 @@ def wmiHotfix(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.hotfix', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.hotfix)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiHotfix',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.hotfix)', 'form': restoredForm}
 
 def wmiService(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.service', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.service)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiService',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.service)', 'form': restoredForm}
 
 def wmiUser(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.user', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.user)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiUser',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.user)', 'form': restoredForm}
 
 def wmiInstalled(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.installed', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.installed)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiInstalled',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.installed)', 'form': restoredForm}
 
 def wmiAutorun(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.autorun', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.autorun)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiAutorun',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.autorun)', 'form': restoredForm}
 
 def wmiProcess(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.process', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.process)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiProcess',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.process)', 'form': restoredForm}
 
 def wmiEnvironment(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.environment', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.environment)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiEnvironment',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.autorun)', 'form': restoredForm}
 
 def wmiShare(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('wmi.share', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(wmi.share)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'wmiShare',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(wmi.share)', 'form': restoredForm}
 
 def asynckeystateLogger(arguments,context,connector):
     restoredForm = lycanthropy.portal.api.restoreForm('asynckeystate.logger', context, arguments)
     if not lycanthropy.portal.api.accessChk(connector,'operator'):
         return {'output': {'error': 'you do not have the correct role to run this command'}, 'context': 'windows(asynckeystate.logger)', 'form': restoredForm}
-    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'agentHalt',lycanthropy.crypto.mkRandom(6))
+    apiResponse = lycanthropy.portal.api.apiBroker().passGeneric(arguments,connector,'asyncKeystatelogger',lycanthropy.crypto.mkRandom(6))
     return {'output': apiResponse.content.decode('utf-8'), 'context': 'windows(asynckeystate.logger)', 'form': restoredForm}
--- svc/ns_daemon.py
+++ svc/ns_daemon.py
@@ -106,30 +106,47 @@ class coreServer():
         )
 
     def makeAuthSuccess(self,unpackedReq):
-        acid = self.messages[unpackedReq['msgID']]['acid']
-        if lycanthropy.auth.cookie.verify(unpackedReq['cookie'], self.sessions[acid]) == False:
-            # return error, encrypted using the acid and nonce associated with the message ID
-            return 1,lycanthropy.daemon.messager.makeRecordArray('{"error":"invalid token"}',
+        try:
+            acid = self.messages[unpackedReq['msgID']]['acid']
+            if lycanthropy.auth.cookie.verify(unpackedReq['cookie'], self.sessions[acid]) == False:
+                # return error, encrypted using the acid and nonce associated with the message ID
+                return 1,lycanthropy.daemon.messager.makeRecordArray('{"error":"invalid token"}',
                                                                 acid,
                                                                 self.messages[unpackedReq['msgID']]['nonce'],
                                                                 self.config['keytypes'][unpackedReq['type']],
                                                                 self.messages[unpackedReq['msgID']],
                                                                 random.choice(self.config['prefixes'])
                                                                )
-        unpackedReq['acid'] = acid
-        return 0,unpackedReq
+            unpackedReq['acid'] = acid
+            return 0,unpackedReq
+        except:
+            print('error making auth success')
+            print(unpackedReq)
+            traceback.print_exc()
 
-    def makeResponseGeneric(self,unpackedReq,msgResponse):
+    def makeResponseGeneric(self,msgStatus,msgResponse):
+#        if self.messages[unpackedReq['msgID']]['garbage'] == 1:
+#            self.messages.pop(unpackedReq['msgID'])
+
         try:
+            if 'nonce' in msgStatus:
+                nonce = msgStatus['nonce']
+            else:
+                nonce = self.messages[msgStatus['msgID']]['nonce']
+            acid = msgStatus['acid']
+            type = msgStatus['type']
             return lycanthropy.daemon.messager.makeRecordArray(
                 str(msgResponse),
-                self.messages[unpackedReq['msgID']]['acid'],
-                self.messages[unpackedReq['msgID']]['nonce'],
-                self.config['keytypes'][unpackedReq['type']],
-                self.messages[unpackedReq['msgID']],
+                acid,
+                nonce,
+                self.config['keytypes'][type],
                 random.choice(self.config['prefixes'])
             )
         except:
+            print('hit exception')
+            print(msgStatus)
+            print(self.messages)
+            traceback.print_exc()
             print('error making generic response')
 
     def makeResponseBuffered(self,unpackedReq,msgResponse):
@@ -163,7 +180,7 @@ class coreServer():
         jsonMsg = json.loads(msgResponse)
         if unpackedReq['acid'] not in self.sessions and 'cookieDough' in jsonMsg:
             self.sessions[unpackedReq['acid']] = jsonMsg['cookieDough']
-        return self.makeResponseGeneric(unpackedReq,msgResponse)
+        return self.makeResponseGeneric(msgStatus,msgResponse)
 
     def dist(self,unpackedReq,msgStatus):
         #ADD BUFFERING FEATURES
@@ -171,7 +188,9 @@ class coreServer():
             return self.makeAuthFail(
                 unpackedReq
             )
+        
         status,referencedReq = self.makeAuthSuccess(unpackedReq)
+        
         if status == 1:
             return referencedReq
 
@@ -190,27 +209,27 @@ class coreServer():
             return self.makeResponseGeneric(referencedReq,{'dist':'ok'})
         elif unpackedReq['pkgID'] == 'PBC':
             self.responseBuffer.pop(referencedReq['distKey'])
-            return self.makeResponseGeneric(referencedReq, '{"index":-1}')
+            return self.makeResponseGeneric(msgStatus, '{"index":-1}')
         elif 'PRR' in unpackedReq['pkgID'].split('|'):
             requiredIndex = unpackedReq['pkgID'].split('|')[1]
             responseBuffer = self.responseBuffer[referencedReq['distKey']]
             if requiredIndex == len(responseBuffer['data']):
                 #segment final, send conclusion
-                return self.makeResponseGeneric(referencedReq, '{"index":-1}')
+                return self.makeResponseGeneric(msgStatus, '{"index":-1}')
             nextBuffer = responseBuffer['data'][int(requiredIndex)]
             msgResponse = {'index':requiredIndex,'data':nextBuffer}
-            return self.makeResponseGeneric(referencedReq, msgResponse)
+            return self.makeResponseGeneric(msgStatus, msgResponse)
         elif unpackedReq['pkgID'] == 'PCR':
             responseBuffer = self.responseBuffer[referencedReq['distKey']]
             #find the next buffer segment and tag it with its position in the buffer
             if responseBuffer['index'] == len(responseBuffer['data']):
                 #segment final, send conclusion
-                return self.makeResponseGeneric(referencedReq, '{"index":-1}')
+                return self.makeResponseGeneric(msgStatus, '{"index":-1}')
             nextBuffer = responseBuffer['data'][int(responseBuffer['index'])]
             msgResponse = {'index':responseBuffer['index'],'data':nextBuffer}
             #pop the buffer segment off the buffer
             self.responseBuffer[referencedReq['distKey']]['index'] += 1
-            return self.makeResponseGeneric(referencedReq, msgResponse)
+            return self.makeResponseGeneric(msgStatus, msgResponse)
 
 
     def heartbeat(self,unpackedReq,msgStatus):
@@ -222,7 +241,7 @@ class coreServer():
         if status == 1:
             return referencedReq
         msgResponse = self.getResponse(referencedReq)
-        return self.makeResponseGeneric(referencedReq,msgResponse)
+        return self.makeResponseGeneric(msgStatus,msgResponse)
 
     def ctrl(self,unpackedReq,msgStatus):
         if 'cookie' not in unpackedReq:
@@ -234,9 +253,10 @@ class coreServer():
             return referencedReq
 
         msgResponse = self.getResponse(referencedReq)
-        return self.makeResponseGeneric(referencedReq,msgResponse)
+        return self.makeResponseGeneric(msgStatus,msgResponse)
 
     def conf(self,unpackedReq,msgStatus):
+        print('status : ' + str(msgStatus))
         if 'cookie' not in unpackedReq:
             return self.makeAuthFail(
                 unpackedReq
@@ -253,7 +273,7 @@ class coreServer():
             #first response is buffer descriptor:
             #{'bufferSize':len(buffer),'bufferKey':msgID}
 
-            return self.makeResponseBuffered(referencedReq, msgResponse)
+            return self.makeResponseBuffered(msgStatus, msgResponse)
         elif confObj[0] == '_PBC':
 
             jsonMsg = json.loads(''.join(self.responseBuffer[confObj[1]]['data']))
@@ -261,7 +281,7 @@ class coreServer():
             cookieDough = self.sessions[referencedReq['acid']]
             self.sessions.pop(referencedReq['acid'])
             self.sessions[jsonMsg['acid']] = cookieDough
-            return self.makeResponseGeneric(referencedReq, '{"index":-1}')
+            return self.makeResponseGeneric(msgStatus, '{"index":-1}')
         elif confObj[0] == '_PCR':
             responseBuffer = self.responseBuffer[confObj[1]]
             #find the next buffer segment and tag it with its position in the buffer
@@ -269,7 +289,7 @@ class coreServer():
             if responseBuffer['index'] == len(responseBuffer['data']):
                 #segment final, send conclusion
 
-                return self.makeResponseGeneric(referencedReq, '{"index":-1}')
+                return self.makeResponseGeneric(msgStatus, '{"index":-1}')
             nextBuffer = responseBuffer['data'][int(responseBuffer['index'])]
             msgResponse = {'index':responseBuffer['index'],'data':nextBuffer}
 
@@ -297,7 +317,7 @@ class coreServer():
             msgResponse = self.getResponse(referencedReq)
         else:
             msgResponse = '{"streamStatus":"continue"}'
-        return self.makeResponseGeneric(referencedReq,msgResponse)
+        return self.makeResponseGeneric(msgStatus,msgResponse)
 
     def getResponse(self, request):
         return lycanthropy.daemon.messager.makeResponse(request,self.gateway)
@@ -323,7 +343,9 @@ class coreServer():
         if msgStatus['action'] == 'update':
             self.messages[unpackedReq['msgID']] = msgStatus
         elif msgStatus['action'] == 'teardown':
-            self.messages.pop(unpackedReq['msgID'])
+            if unpackedReq['msgID'] in self.messages:
+                print("popping : " + str(self.messages[unpackedReq['msgID']]))
+                self.messages.pop(unpackedReq['msgID'])
         #print('processed:' + processedMessage)
         return processedMessage
 
@@ -349,6 +371,7 @@ class coreServer():
                 ]
             )
         except:
+            traceback.print_exc()
             replyData = ['::1']
         for rdata in replyData:
             reply.add_answer(RR(rname=qn, rtype=QTYPE.AAAA, rclass=1, ttl=self.TTL, rdata=AAAA(rdata)))
